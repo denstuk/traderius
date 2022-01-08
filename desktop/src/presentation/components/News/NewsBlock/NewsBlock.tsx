c5c3fd4edbcb0ad8fcaf9dc636ab08e35d952f83
DenStuk <den.stuk00@gmail.com>
2022-01-08 05:24:52 +0300

    refactor(desktop): add more styling for news

--- desktop/src/presentation/components/News/NewsBlock/NewsBlock.tsx
+++ desktop/src/presentation/components/News/NewsBlock/NewsBlock.tsx
@@ -8,8 +8,9 @@ export const NewsBlock: React.FC<INewsBlockProps> = ({ news }: INewsBlockProps) 
     return (
         <div className="news-block">
             <h3 className="news-block__date">{news.pubDate.split("T")[0]}</h3>
-            <h2 className="news-block__subtitle">{news.title}</h2>
-            <a href={news.link}>Подробнее</a>
+            <h2 className="news-block__publisher">{news.source}</h2>
+            <h2 className="news-block__title">{news.title}</h2>
+            <a className="news-block__link" href={news.link}>Подробнее</a>
         </div>
     )
 };
