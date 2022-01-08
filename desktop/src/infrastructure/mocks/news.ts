8335e8c5b8c7bb8533a54d4d5c02d47e921a0581
DenStuk <den.stuk00@gmail.com>
2022-01-08 04:37:04 +0300

    feat(desktop): add news mocker

---
+++ desktop/src/infrastructure/mocks/news.ts
@@ -0,0 +1,20 @@
+import {INews} from "../../domain";
+
+export class NewsMocker {
+    private static index: number = 0;
+
+    static mock(amount: number): INews[] {
+        const news: INews[] = [];
+        for (let i = 0; i < amount; i++) {
+            news.push({
+                id: String(NewsMocker.index),
+                title: "News name example " + NewsMocker.index,
+                link: "https://google.com",
+                pubDate: new Date().toDateString(),
+                source: "Shelby News Paper LTD"
+            });
+            NewsMocker.index += 1;
+        }
+        return news;
+    }
+}
