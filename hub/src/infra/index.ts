8f7a38a3efc15325f6ec581d89eef7a47f457c89
DenStuk <den.stuk00@gmail.com>
2022-02-09 11:43:43 +0300

    refactor(hub): add root infra file

---
+++ hub/src/infra/index.ts
@@ -0,0 +1,3 @@
+export * from "./configuration";
+export * from "./ioc";
+export * from "./markets";
--- hub/src/infra/ioc.ts
+++ hub/src/infra/ioc.ts
@@ -1,18 +1,19 @@
 import * as inversify from "inversify";
+import { AuthController } from "../app/http/controllers/auth.controller";
 import { PasswordService } from "../domain/shared/password.service";
 import { TokenService } from "../domain/shared/token.service";
-import { AuthService } from "../domain/users/auth/auth.service";
-import { UserService } from "../domain/users/user.service";
+import { UserService } from "../domain/users/services/user.service";
 import { Redis } from "./redis";
 
 const container = new inversify.Container();
 
-/* Domain services */
+/* Services */
 container.bind<PasswordService>(PasswordService).toSelf().inSingletonScope();
 container.bind<TokenService>(TokenService).toSelf().inSingletonScope();
-container.bind<AuthService>(AuthService).toSelf().inSingletonScope();
 container.bind<Redis>(Redis).toSelf().inSingletonScope();
-
 container.bind<UserService>(UserService).toSelf().inSingletonScope();
+
+/* Controllers */
+container.bind<AuthController>(AuthController).toSelf().inRequestScope();
 
 export { container as ioc };
