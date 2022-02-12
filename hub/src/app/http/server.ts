47e16d38621d22d2819b9844555b16ac89b916f7
DenStuk <den.stuk00@gmail.com>
2022-02-12 19:00:03 +0300

    refactor(hub): reorganize server

--- hub/src/app/http/server.ts
+++ hub/src/app/http/server.ts
@@ -4,26 +4,34 @@ import "express-async-errors";
 import { Logger } from "../../infra/logger";
 import { HttpRouter } from "./router";
 import { ErrorGuard } from "./guards/error.guard";
-import { Configuration } from "../../infra/configuration/configuration";
+import { Configuration } from "../../infra";
 
 export class HttpServer {
-    private static server: http.Server | undefined;
+	private static server: http.Server | undefined;
 
-    static async up(): Promise<void> {
-        const app = express();
-        app.use(express.json({ limit: "20mb" }));
-        HttpRouter.register(app);
-        app.use(ErrorGuard);
-        this.server = http.createServer(app);
-        this.server.listen(Configuration.get<number>("Port"), () => Logger.info("Server listening"));
-    }
+	static async up(): Promise<void> {
+		const host = Configuration.get<string>("Host");
+		const port = Configuration.get<number>("Port");
 
-    static async down(): Promise<void> {
-        if (this.server) {
-            return new Promise<void>(res => {
-                this.server!.close(() => Logger.info("Server shutdowned"));
-                res();
-            })
-        }
-    }
+		const app = this.configure(express());
+		this.server = http.createServer(app);
+
+		this.server.listen(port, () => Logger.info(`Server started on http://${host}:${port}`));
+	}
+
+	static async down(): Promise<void> {
+		if (this.server) {
+			return new Promise<void>((res) => {
+				this.server!.close(() => Logger.info("Server shutdowned"));
+				res();
+			});
+		}
+	}
+
+	private static configure(app: express.Application): express.Application {
+		app.use(express.json({ limit: "20mb" }));
+		HttpRouter.register(app);
+		app.use(ErrorGuard);
+		return app;
+	}
 }
