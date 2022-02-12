b2aedf1d8a3869380548d3b95c03c910d0f489ab
DenStuk <den.stuk00@gmail.com>
2022-02-12 18:36:51 +0300

    style(web): add prettier

--- hub/src/application.ts
+++ hub/src/application.ts
@@ -4,16 +4,16 @@ import { Logger } from "./infra/logger";
 import { Redis } from "./infra/redis";
 
 export class Application {
-    static async up(): Promise<void> {
-        Logger.info("Application started");
-        await Database.connect();
-        await Redis.connect();
-        await HttpServer.up();
-    }
+	static async up(): Promise<void> {
+		Logger.info("Application started");
+		await Database.connect();
+		await Redis.connect();
+		await HttpServer.up();
+	}
 
-    static async down(): Promise<void> {
-        await HttpServer.down();
-        await Database.disconnect();
-        Logger.info("Application closed");
-    }
+	static async down(): Promise<void> {
+		await HttpServer.down();
+		await Database.disconnect();
+		Logger.info("Application closed");
+	}
 }
