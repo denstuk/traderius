e981dceaf8b5df45694df47434276fbbba0322fc
DenStuk <den.stuk00@gmail.com>
2022-02-12 19:00:29 +0300

    feat(hub): add other regression support

---
+++ hub/src/domain/analysis/analysis.types.ts
@@ -0,0 +1,3 @@
+export type Index = number;
+export type Price = number;
+export type PricePoint = [Index, Price]; // (x, y)
--- hub/src/domain/analysis/regressor.ts
+++ hub/src/domain/analysis/regressor.ts
@@ -1,17 +1,26 @@
 import regression from "regression";
+import { PricePoint } from "./analysis.types";
+import { injectable } from "inversify";
 
-type Price = number;
-type Index = number;
-type PricePoint = [Index, Price];
-
+@injectable()
 export class Regressor {
-	calculateLinear(points: [number, number][]): [number, number] {
+	calculateLinear(points: PricePoint[]): PricePoint {
 		const regressor = regression.linear(points);
 		return regressor.predict(points.length);
 	}
 
 	calculatePolynomial(points: PricePoint[]): PricePoint {
-		const regressor = regression.polynomial(points)
+		const regressor = regression.polynomial(points);
+		return regressor.predict(points.length);
+	}
+
+	calculatePower(points: PricePoint[]): PricePoint {
+		const regressor = regression.power(points);
+		return regressor.predict(points.length);
+	}
+
+	calculateExponential(points: PricePoint[]): PricePoint {
+		const regressor = regression.exponential(points);
 		return regressor.predict(points.length);
 	}
 }
