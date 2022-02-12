a884392c1bd3a80d721c1e85078d00cf37a5bcbf
DenStuk <den.stuk00@gmail.com>
2022-02-12 19:01:33 +0300

    feat(hub): add strategy field for user entity

--- hub/src/domain/users/entities/user.entity.ts
+++ hub/src/domain/users/entities/user.entity.ts
@@ -1,23 +1,27 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 import { ApplicationEntity } from "../../shared/entities/base.entity";
+import { UserStrategy } from "../users.types";
 
 @Entity({ name: "users" })
 export class UserEntity extends ApplicationEntity {
-    @PrimaryGeneratedColumn("uuid")
-    id!: string;
+	@PrimaryGeneratedColumn("uuid")
+	id!: string;
 
-    @Column("varchar", { nullable: false })
-    login!: string;
+	@Column("varchar", { nullable: false, unique: true })
+	login!: string;
 
-    @Column("varchar", { nullable: false })
-    email!: string;
+	@Column("varchar", { nullable: false, unique: true })
+	email!: string;
 
-    @Column("varchar", { nullable: false })
-    password!: string;
+	@Column("varchar", { nullable: false })
+	password!: string;
 
 	@Column("varchar", { nullable: false })
 	salt!: string;
 
-	@Column("varchar", { nullable: true, name: 'tinkoff_market_token' })
+	@Column("enum", { enum: UserStrategy, nullable: false, default: UserStrategy.None })
+	strategy!: UserStrategy;
+
+	@Column("varchar", { nullable: true, name: "tinkoff_market_token" })
 	tinkoffMarketToken!: string;
 }
---
+++ hub/src/domain/users/users.types.ts
@@ -0,0 +1,10 @@
+export enum UserStrategy {
+	None = 1,
+	Automated
+}
+
+export interface IUpdateUser {
+	tinkoffMarketToken?: string;
+	login?: string;
+	strategy?: UserStrategy;
+}
