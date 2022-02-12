ca388f0d912b08647af943c0c96a3f65a57e5eea
DenStuk <den.stuk00@gmail.com>
2022-02-12 19:18:11 +0300

    feat(web): add NewsCard component

--- web/src/presentation/components/NewsCard/NewsBlock/NewsBlock.tsx
+++ web/src/presentation/components/NewsCard/NewsBlock/NewsBlock.tsx
@@ -1,13 +1,14 @@
-import * as React from "react";
+import React from "react";
 import "./NewsBlock.css";
-import { INews } from "../../../../domain";
+import type { INews } from "../../../../domain";
+import dayjs from "dayjs";
 
 type INewsBlockProps = { news: INews; }
 
 export const NewsBlock: React.FC<INewsBlockProps> = ({ news }: INewsBlockProps) => {
     return (
         <div className="news-block">
-            <h3 className="news-block__date">{news.pubDate.split("T")[0]}</h3>
+            <h3 className="news-block__date">{dayjs(news.pubDate).format("MMMM D, YYYY hh:mm")}</h3>
             <h2 className="news-block__publisher">{news.source}</h2>
             <h2 className="news-block__title">{news.title}</h2>
             <a className="news-block__link" href={news.link}>Подробнее</a>
---
+++ web/src/presentation/components/NewsCard/NewsCard.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+import "./NewsCard.scoped.sass";
+import { NewsBlock } from "./NewsBlock/NewsBlock";
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { INews, useApi } from "../../../domain";
+import {PuffLoader} from "../PuffLoader/PuffLoader";
+
+export const NewsCard: React.FC = () => {
+    const { loading, data } = useApi<INews[]>({ url: "http://localhost:9801/api/v1/news" });
+    const [position, setPosition] = React.useState(0);
+    const isNewsLoaded = !loading && data && data.length !== 0;
+
+    const togglePosition = (direction: 'prev' | 'next'): void => {
+        if (direction === 'prev') return setPosition(position === 0 ? 0 : position - 1);
+        if (direction === 'next') return setPosition(position === data.length - 1 ? data.length - 1 : position + 1)
+    }
+    
+    return (
+        <div className="news">
+            { isNewsLoaded ? <NewsBlock news={data[position]} /> : <PuffLoader isLoading={loading} /> }
+            <div className="news__toggle-bar">
+                <button onClick={() => togglePosition('prev')} className="news__toggle-btn"><FontAwesomeIcon icon={faChevronLeft} /></button>
+                <button onClick={() => togglePosition('next')} className="news__toggle-btn"><FontAwesomeIcon icon={faChevronRight} /></button>
+            </div>
+        </div>
+    )
+}
