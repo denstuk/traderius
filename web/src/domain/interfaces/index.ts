a7871f6cb32fccf3b1fcd9f293ab5872b28601d5
DenStuk <den.stuk00@gmail.com>
2022-02-19 14:15:10 +0300

    feat(web): add interfaces

--- web/src/domain/interfaces/index.ts
+++ web/src/domain/interfaces/index.ts
@@ -5,3 +5,26 @@ export interface INews {
     pubDate: string;
     source: string;
 }
+
+export interface IUser {
+    id: string;
+    login: string;
+    email: string;
+}
+
+export interface IBalance {
+    usd: number;
+    eur: number;
+    rub: number;
+}
+
+export interface ICandle {
+    o: number;
+    c: number;
+    h: number;
+    l: number;
+    v: number;
+    time: Date;
+    interval: string;
+    figi: string;
+}
