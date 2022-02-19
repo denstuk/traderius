a48d5b92f8d09eacbfbbb3b5e451426778fcbfac
DenStuk <den.stuk00@gmail.com>
2022-02-19 14:19:19 +0300

    feat(web): add store connection, toast provider

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./presentation/App";
+import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import { store } from "./domain";
+import dayjs from "dayjs";
 import "./presentation/styles/index.css";
 import "./presentation/styles/index.sass";
-import App from "./presentation/App";
-import dayjs from "dayjs";
+import "react-toastify/dist/ReactToastify.css";
 import "dayjs/locale/ru";
 
 dayjs.locale("ru");
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+			<ToastContainer />
+		</Provider>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
