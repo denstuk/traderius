0c22588b44c3cc80412a4cd808c609ea7071374d
DenStuk <den.stuk00@gmail.com>
2022-02-19 14:00:08 +0300

    feat(hub): add predictor for analysis controller

--- hub/src/app/http/controllers/analysis.controller.ts
+++ hub/src/app/http/controllers/analysis.controller.ts
@@ -2,21 +2,26 @@ import { inject, injectable } from "inversify";
 import { Regressor } from "../../../domain/analysis/regressor";
 import { Configuration, TinkoffMarket } from "../../../infra";
 import { PricePoint } from "../../../domain/analysis/analysis.types";
+import { Predictor } from "../../../infra";
 
 @injectable()
 export class AnalysisController {
-	constructor(@inject(Regressor) private regressor: Regressor) {}
+	constructor(
+		@inject(Regressor) private regressor: Regressor,
+		@inject(Predictor) private predictor: Predictor
+	) {}
 
 	async analysis({ ticker }: { ticker: string }): Promise<{
 		points: PricePoint[];
 		regressions: { linear: PricePoint; polynomial: PricePoint; };
-		lstm: PricePoint;
+		lstm: { lstm30: number, lstm7: number };
 	}> {
 		const market = new TinkoffMarket(Configuration.get("TinkoffSharedToken"));
 		const historical = await market.history(ticker);
 		const points: PricePoint[] = historical.candles.map((h, i) => [h.c, i]);
 		const linear = this.regressor.calculateLinear(points);
 		const polynomial = this.regressor.calculatePolynomial(points);
-		return { points, regressions: { linear, polynomial }, lstm: [2, 2] };
+		const lstmResult = await this.predictor.predict(points.map((p) => p[0]));
+		return { points, regressions: { linear, polynomial }, lstm: { ...lstmResult.prediction } };
 	}
 }
