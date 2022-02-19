39b20f25c7984d4760fbe57acbd3c11e250b1c08
DenStuk <den.stuk00@gmail.com>
2022-02-19 13:57:02 +0300

    feat(hub): add thread

---
+++ hub/src/infra/thread/thread.ts
@@ -0,0 +1,10 @@
+import {injectable} from "inversify";
+
+@injectable()
+export class Thread {
+	async sleep(ms: number): Promise<void> {
+		return new Promise<void>((resolve) => {
+			setTimeout(() => resolve(), ms);
+		});
+	}
+}
