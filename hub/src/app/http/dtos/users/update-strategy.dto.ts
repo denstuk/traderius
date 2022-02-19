9142e423267a65081abd4b48b43631ce5cacd0e3
DenStuk <den.stuk00@gmail.com>
2022-02-19 13:58:35 +0300

    refactor(hub): restructure dtos

---
+++ hub/src/app/http/dtos/users/update-strategy.dto.ts
@@ -0,0 +1,8 @@
+import {IsDefined, IsEnum} from "class-validator";
+import {UserStrategy} from "../../../../domain/users/users.types";
+
+export class UpdateStrategyDto {
+	@IsDefined()
+	@IsEnum(UserStrategy)
+	automatedTradingEnabled!: UserStrategy;
+}
