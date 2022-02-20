574522b721590c3e5e5bb82571151790de291215
DenStuk <den.stuk00@gmail.com>
2022-02-20 12:06:41 +0300

    refactor(web): add redirect to auth

--- web/src/presentation/components/Header/Header.tsx
+++ web/src/presentation/components/Header/Header.tsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./Header.scoped.sass";
 import { faBell } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {Link, useNavigate} from "react-router-dom";
+import { useAuth } from "../../../domain/hooks/useAuth";
 
 export const Header: React.FC = () => {
+	const navigate = useNavigate();
+	const { auth } = useAuth();
+
+	useEffect(() => {
+		auth().then((authorized) => !authorized && navigate("/auth"));
+	}, []);
+
 	return (
 		<header>
-			<div className="title">Traderius</div>
-			<FontAwesomeIcon icon={faBell} />
+			<div className="title"><Link to={"/"}>Traderius</Link></div>
+			<Link to={"/user"}><FontAwesomeIcon icon={faBell} /></Link>
 		</header>
 	);
 };
