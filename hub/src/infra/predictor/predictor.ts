a016d1e6ea1baef9e01dc55a41efd3f5b66c04db
DenStuk <den.stuk00@gmail.com>
2022-02-20 12:37:50 +0300

    fix(hub): change predictor location

--- hub/src/infra/predictor/predictor.ts
+++ hub/src/infra/predictor/predictor.ts
@@ -5,7 +5,7 @@ import {injectable} from "inversify";
 @injectable()
 export class Predictor {
 	async predict(values: number[]): Promise<PredictionResponse> {
-		const response = await axios({ url: "http://127.0.0.1:8000/predict", method: "POST", data: { values } });
+		const response = await axios({ url: "http://127.0.0.1:9803/predict", method: "POST", data: { values } });
 		return response.data as PredictionResponse;
 	}
 }
