593bde34eb83977b35178b3ff8e9b7d68617baba
DenStuk <den.stuk00@gmail.com>
2022-02-20 12:08:38 +0300

    feat(web): add api usage for users-page

--- web/src/presentation/pages/UserPage/UserPage.tsx
+++ web/src/presentation/pages/UserPage/UserPage.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import "./UserPage.scoped.sass";
-import {PageTitle} from "../../components/PageTitle/PageTitle";
+import { PageTitle } from "../../components/PageTitle/PageTitle";
+import { UserBalanceBadge } from "./UserBalanceBadge/UserBalanceBadge";
+import { IBalance, useAppSelector } from "../../../domain";
+import { UserInfoBadge } from "./UserInfoBadge/UserInfoBadge";
 
 export const UserPage: React.FC = () => {
+    const state = useAppSelector((state) => state.authReducer);
+    const balance: IBalance = { rub: 1000, usd: 600, eur: 5 };
+
     return <div className="user-page">
         <PageTitle text="Личный кабинет" />
         <div className="user-page__form-wrapper">
             <div className="user-page__form">
-                <div>Denis Stuk</div>
+                <UserBalanceBadge balance={balance} />
+                { state.user && <UserInfoBadge user={state.user} /> }
             </div>
         </div>
     </div>
