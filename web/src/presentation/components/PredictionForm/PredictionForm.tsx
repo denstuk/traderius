f39768d75e55952a7ba6c6200a76bd03c3d4c70d
DenStuk <den.stuk00@gmail.com>
2022-02-20 12:07:19 +0300

    refactor(web): change styles, implement search for analysis

--- web/src/presentation/components/PredictionForm/PredictionForm.tsx
+++ web/src/presentation/components/PredictionForm/PredictionForm.tsx
@@ -1,14 +1,20 @@
 import * as React from "react";
 import "./PredictionForm.scoped.sass";
 
-export const PredictionForm: React.FC = (): React.ReactElement => {
+type PredictionFormProps =  { setTicker: (value: string) => void; ticker: string; onAnalysisClick: () => Promise<void> }
+
+export const PredictionForm: React.FC<PredictionFormProps> = (props: PredictionFormProps): React.ReactElement => {
 	return (
 		<div className="prediction-form">
 			<div className="prediction-form__search">
 				<h2>Введите тикер ценной бумаги:</h2>
-				<input placeholder="AAPL" />
+				<input
+					placeholder="AAPL"
+					onChange={(e) => props.setTicker(e.target.value)}
+					value={props.ticker}
+				/>
 			</div>
-			<button>Анализ</button>
+			<button onClick={() => props.onAnalysisClick()}>Анализ</button>
 		</div>
 	);
 };
